b04fce05600901f95c0bed2eb5ff53ad85b117a9
Alex Cooksey <aecooksey2651@gmail.com>
2021-11-03 10:47:12 -0400

    Added show more button JavaScript

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ h2.textContent = "Starter's Guide";
 
 document.querySelector("body header#header").appendChild(h2);
 
+document.getElementById("button").addEventListener('click', showMoreText)
+
+function showMoreText() {
+    document.getElementById("showMore").style.display = "block";
+}
+
+
